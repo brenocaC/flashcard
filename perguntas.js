5ba3d3e078670af1d975c8878dfec0104b65aef4
breninho <00001060632378sp@al.educacao.sp.gov.br>
2024-11-19 15:55:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual é o melhor time do Brasil?',
+    'O Corinthians'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo passa em los santos?',
+    'Gta V'
+)
+
+criaCartao(
+    'Academia',
+    'Qual exercicio se faz na academia para costas?',
+    'Remada'
+)
